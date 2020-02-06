287e02418accdf7bac14c16f04885c49a96b9ec7
Marcell <banuma@freemail.hu>
2020-02-06 12:39:15 +0000

    setup SASS folder structure and Gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,12 @@
-require ('bootstrap')
+
+let gulp = require('gulp');
+let rename = require('gulp-rename');
+let sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+	return gulp.src('./scss/styles.scss')
+		.pipe(sass())
+		.pipe(rename('style.css'))
+		.pipe(gulp.dest('./css/'));
+});
+
